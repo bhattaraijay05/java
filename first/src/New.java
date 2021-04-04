48cb02d6845b5cb4b71c4c49d9e01188576ca5d5
bhattaraijay05 <bhattaraijay23@gmail.com>
2021-04-04 18:05:36 +0530

    basics

---
+++ first/src/New.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class New {
+    int x = 5;
+
+    public static void main(String[] args) {
+        New myObj = new New();
+        System.out.println(myObj.x);
+        function();
+        calc();
+    }
+
+    static void function() {
+        System.out.println("I am a function!");
+    }
+
+    static void calc() {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter the first number");
+        int a = scan.nextInt();
+        System.out.println("Enter the second number");
+        int b = scan.nextInt();
+        try {
+            System.out.println("The addition is " + (a / b));
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        scan.close();
+    }
+}
