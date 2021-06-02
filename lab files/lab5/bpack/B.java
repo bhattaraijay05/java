3ab25b81cec63f084a24986b9576dbab5908bf77
bhattaraijay05 <bhattaraijay23@gmail.com>
2021-06-02 13:16:06 +0530

    lab5

--- lab files/lab5/bpack/B.java
+++ lab files/lab5/bpack/B.java
@@ -5,9 +5,9 @@ import apack.*;
 
 public class B extends A {
     public void display() {
-        // System.out.println("NUM1 : " + A._num1);
-        // System.out.println("NUM2 : " + A.num2);
-        // System.out.println("NUM3 : " + A.num3);
-        // System.out.println("NUM4 : " + A.num4);
+        System.out.println("NUM1 : " + A._num1);
+        System.out.println("NUM2 : " + A.num2);
+        System.out.println("NUM3 : " + A.num3);
+        System.out.println("NUM4 : " + A.num4);
     }
 }
