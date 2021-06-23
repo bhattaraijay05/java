88a487f50c7fbb986300a114c28775f8f63399be
bhattaraijay05 <bhattaraijay23@gmail.com>
2021-06-23 11:20:52 +0530

    lab

--- lab files/lab5/cpack/C.java
+++ lab files/lab5/cpack/C.java
@@ -5,9 +5,9 @@ import apack.A;
 public class C {
     public void display() {
         A a = new A();
-        System.out.println(a._num1);
-        System.out.println(a.num2);
-        System.out.println(a.num3);
+        // System.out.println(a._num1);
+        // System.out.println(a.num2);
+        // System.out.println(a.num3);
         System.out.println(a.num4);
     }
 }
